9537e0f17db526edad1c0dbb8fd7d967800ecdfa
Alejandro Ismael Silva <alex.sai95@gmail.com>
2016-10-13 12:06:06 -0300

    Changed app name

--- Watch2Gether/Watch2Gether/urls.py
+++ Watch2Gether/Watch2Gether/urls.py
@@ -1,4 +1,4 @@
-"""videochat URL Configuration
+"""Watch2Gether URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- Watch2Gether/Watch2Gether/wsgi.py
+++ Watch2Gether/Watch2Gether/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for videochat project.
+WSGI config for Watch2Gether project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "videochat.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Watch2Gether.settings")
 
 application = get_wsgi_application()

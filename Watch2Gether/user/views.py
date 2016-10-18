a59f84dba14f5e5ab41848c8d20fdada5957bf3c
Mauricio Guignard <mauguignard@gmail.com>
2016-10-18 20:18:22 -0300

    Removed redundant get_or_create() method calls

--- Watch2Gether/user/views.py
+++ Watch2Gether/user/views.py
@@ -7,26 +7,20 @@ from .models import Friendship, FriendshipRequest
 
 @login_required
 def index(request):
-    user_friendship, _ = Friendship.objects.get_or_create(user=request.user)
-
-    friendship_requests_list = user_friendship.getFriendshipRequests()
+    friendship_requests_list = request.user.friendship.getFriendshipRequests()
     context = {'friendship_requests_list': friendship_requests_list}
     return render(request, 'user/index.html', context)
 
 @login_required
 def friends(request):
-    user_friendship, _ = Friendship.objects.get_or_create(user=request.user)
-
-    friendship_list = user_friendship.friends.all()
+    friendship_list = request.user.friendship.friends.all()
     context = {'friendship_list': friendship_list}
     return render(request, 'user/friends.html', context)
 
 @login_required
 def friendship_accept(request):
-    user_friendship, _ = Friendship.objects.get_or_create(user=request.user)
-
     try:
-        friendship_request = user_friendship.getFriendshipRequests().get(id=request.GET['id'])
+        friendship_request = request.user.friendship.getFriendshipRequests().get(id=request.GET['id'])
     except (KeyError, FriendshipRequest.DoesNotExist):
         return HttpResponseRedirect(reverse('user:index'))
 
@@ -35,10 +29,8 @@ def friendship_accept(request):
 
 @login_required
 def friendship_reject(request):
-    user_friendship, _ = Friendship.objects.get_or_create(user=request.user)
-
     try:
-        friendship_request = user_friendship.getFriendshipRequests().get(id=request.GET['id'])
+        friendship_request = request.user.friendship.getFriendshipRequests().get(id=request.GET['id'])
     except (KeyError, FriendshipRequest.DoesNotExist):
         return HttpResponseRedirect(reverse('user:index'))
 

27d54f2310730f3b41a812464369ba88b963f363
Mauricio Guignard <mauguignard@gmail.com>
2016-10-18 20:27:19 -0300

    Fixed PEP8 code style in user/signals.py

--- Watch2Gether/user/signals.py
+++ Watch2Gether/user/signals.py
@@ -4,7 +4,8 @@ from django.dispatch import receiver
 
 from .models import Friendship
 
-@receiver(post_save, sender = settings.AUTH_USER_MODEL)
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_friendship(sender, instance, created, **kwargs):
     if created:
-        Friendship.objects.create(user = instance)
+        Friendship.objects.create(user=instance)

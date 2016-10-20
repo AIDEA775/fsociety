dd817d99a3b672276d653914ec4433d8dfef4aca
Alejandro Ismael Silva <alex.sai95@gmail.com>
2016-10-19 23:26:26 -0300

    Added 3 test to login

--- Watch2Gether/login/tests.py
+++ Watch2Gether/login/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.db.utils import IntegrityError
+from .models import CustomUser
 
-# Create your tests here.
+def signup(username, email, password):
+    CustomUser.objects.create_user(username, email, password)
+
+class LoginUser(TestCase):
+    def test_login_user_and_test_home_page(self):
+        """
+        Create new user an save this in database.
+        """
+        signup("username", "example@example", "password")
+        self.client.login(username="username", password="password")
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, "Bienvenido")
+
+    def test_signup_user_with_duplicate_email(self):
+        """
+        Create two user with the same email.
+        """
+        signup("username", "user@user.com", "password")
+        try:
+            signup("otheruser", "user@user.com", "password")
+            self.fail('Two user saved!')
+        except IntegrityError:
+            pass
+
+    def test_signup_user_with_duplicate_username(self):
+        """
+        Create two user with the same username.
+        """
+        signup("username", "user@user.com", "password")
+        try:
+            signup("username", "other@other.com", "password")
+            self.fail('Two user saved!')
+        except IntegrityError:
+            pass

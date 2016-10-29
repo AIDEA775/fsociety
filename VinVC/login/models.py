b29248aadac3e93455841f97399eea896b7e5e40
Alejandro Ismael Silva <alex.sai95@gmail.com>
2016-10-29 13:09:41 -0300

    Add email facebook scope

--- VinVC/login/models.py
+++ VinVC/login/models.py
@@ -7,7 +7,7 @@ class CustomUser(AbstractUser):
     A custom user model with unique email field
     """
     email = models.CharField(max_length=40, unique=True)
-    first_name = models.CharField(max_length=20, default=None)
-    last_name = models.CharField(max_length=20, default=None)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
 
     REQUIRED_FIELDS = ['email', 'first_name', 'last_name']

6609dff7bfa7625f4d5ac1d79dbbec0dede29f0b
Alejandro Ismael Silva <alex.sai95@gmail.com>
2016-10-29 21:47:11 -0300

    Improved tests of login

--- VinVC/login/tests.py
+++ VinVC/login/tests.py
@@ -1,52 +1,67 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.test import Client
 from django.db.utils import IntegrityError
-from .models import CustomUser
+from django.contrib.auth import get_user, get_user_model
 
-def signup(username, email, password):
-    CustomUser.objects.create_user(username, email, password,
-        first_name="first", last_name="last")
+
+def create_user(username="username", email="user@example.com", password="pass",
+                first_name="first", last_name="last"):
+    return get_user_model().objects.create_user(username, email, password,
+        first_name=first_name, last_name=last_name)
+
+
+def login_user(username="username", password="pass"):
+    c = Client()
+    c.login(username=username, password=password)
+    return c
+
 
 class LoginUser(TestCase):
-    def test_login_user_and_test_home_page(self):
+    def test_login_user(self):
         """
-        Create new user an save this in database.
+        Create new user and check if is authenticated.
         """
-        signup("username", "example@example", "password")
-        self.client.login(username="username", password="password")
-        response = self.client.get(reverse('user:index'))
-        self.assertContains(response, "Bienvenido")
+        create_user()
+        self.client = login_user()
+        user = get_user(self.client)
+        assert user.is_authenticated()
 
-    def test_signup_user_with_duplicate_email(self):
+    def test_bad_login_user(self):
+        create_user()
+        self.client = login_user(password="badpass")
+        user = get_user(self.client)
+        assert not user.is_authenticated()
+
+    def test_create_user_with_duplicate_email(self):
         """
-        Create two user with the same email.
+        Create two users with the same email.
         """
-        signup("username", "user@user.com", "password")
+        create_user()
         try:
-            signup("otheruser", "user@user.com", "password")
-            self.fail('Two user saved!')
+            create_user(username="otheruser")
+            self.fail('Two users saved')
         except IntegrityError:
             pass
 
-    def test_signup_user_with_duplicate_username(self):
+    def test_create_user_with_duplicate_username(self):
         """
-        Create two user with the same username.
+        Create two users with the same username.
         """
-        signup("username", "user@user.com", "password")
+        create_user()
         try:
-            signup("username", "other@other.com", "password")
-            self.fail('Two user saved!')
+            create_user(email="other@example.com")
+            self.fail('Two users saved')
         except IntegrityError:
             pass
 
-    def test_count_user_signup(self):
+    def test_count_users_created(self):
         """
-        Create many user and check counter.
+        Create many user and check number of users.
         """
-        self.assertEqual(CustomUser.objects.count(), 0)
-        signup("username", "user@user.com", "password")
-        self.assertEqual(CustomUser.objects.count(), 1)
-        signup("username1", "user1@user.com", "password")
-        self.assertEqual(CustomUser.objects.count(), 2)
-        signup("username2", "user2@user.com", "password")
-        self.assertEqual(CustomUser.objects.count(), 3)
+        self.assertEqual(get_user_model().objects.count(), 0)
+        create_user()
+        self.assertEqual(get_user_model().objects.count(), 1)
+        create_user(username="username1", email="user1@example.com")
+        self.assertEqual(get_user_model().objects.count(), 2)
+        create_user(username="username2", email="user2@example.com")
+        self.assertEqual(get_user_model().objects.count(), 3)

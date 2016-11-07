4efd30167dad2de6ff6b8bc217e1934fc513cc59
Alejandro Ismael Silva <alex.sai95@gmail.com>
2016-11-07 14:50:06 -0300

    Removed index and friend_videos views of video

--- VinVC/video/urls.py
+++ VinVC/video/urls.py
@@ -4,10 +4,8 @@ from . import views
 
 app_name = 'video'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^upload/$', views.upload, name='upload'),
-    url(r'^uploaded/delete/$', views.delete, name='delete'),
-    url(r'^friends_videos/$', views.friends_videos, name='friends_videos'),
     url(r'^feed/$', views.feed, name='feed'),
+    url(r'^upload/$', views.upload, name='upload'),
+    url(r'^delete/$', views.delete, name='delete'),
     url(r'^(?P<video_id>[0-9]+)/$', views.player, name='player'),
 ]
--- VinVC/video/views.py
+++ VinVC/video/views.py
@@ -8,10 +8,16 @@ from .models import Video, WatchingVideo
 
 
 @login_required
-def index(request):
-    videos = Video.objects.all()
-    context = {'videos': videos}
-    return render(request, 'video/index.html', context)
+def feed(request):
+    friendship_list = request.user.friendship.get_friends()
+    watched_videos = WatchingVideo.objects.filter(user__friendship__in=
+                                                  friendship_list).\
+        values('video')
+    videos = Video.objects.filter(pk__in=watched_videos)
+    must_viewed = Video.objects.order_by('views')[:10]
+    watching = videos | must_viewed
+    context = {'watching': watching}
+    return render(request, "video/feed.html", context)
 
 
 @login_required
@@ -52,27 +58,6 @@ def delete(request):
         return redirect('user:uploaded', user_id=request.user.id)
     else:
         return HttpResponseForbidden("Don't you have permission to delete")
-
-
-@login_required
-def friends_videos(request):
-    friendship_list = request.user.friendship.get_friends()
-    videos = Video.objects.filter(author__friendship__in=friendship_list)
-    context = {'videos': videos}
-    return render(request, "video/friends_videos.html", context)
-
-
-@login_required
-def feed(request):
-    friendship_list = request.user.friendship.get_friends()
-    watched_videos = WatchingVideo.objects.filter(user__friendship__in=
-                                                  friendship_list).\
-        values('video')
-    videos = Video.objects.filter(pk__in=watched_videos)
-    must_viewed = Video.objects.order_by('views')[:10]
-    watching = videos | must_viewed
-    context = {'watching': watching}
-    return render(request, "video/feed.html", context)
 
 
 @login_required

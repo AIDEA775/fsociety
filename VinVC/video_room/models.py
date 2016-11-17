ae87dce25f0fd1e27af1e80c885643bd2ec30bef
Agustín Curto <agucurto95@gmail.com>
2016-11-17 13:36:41 -0300

    Added models to video_room

--- VinVC/video_room/models.py
+++ VinVC/video_room/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from video.models import Video
+from chat_room.models import Comment
 
-# Create your models here.
+
+class VideoRoom(models.Model):
+    videos = models.ManyToManyField(Video,
+                                    through='VRVideos',
+                                    through_fields=('room', 'video'),
+                                    symmetrical=True,
+                                    related_name='video_room')
+
+    chat_room = models.ForeignKey(Comment,
+                                  on_delete=models.CASCADE)
+
+
+class VRVideos(models.Model):
+    room = models.ForeignKey(VideoRoom,
+                             on_delete=models.CASCADE,
+                             related_name='room')
+    video = models.ForeignKey(Video,
+                              on_delete=models.CASCADE,
+                              related_name='video')

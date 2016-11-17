e387b7fa0a29051dff33db08d87103542459c7d8
Agustín Curto <agucurto95@gmail.com>
2016-11-16 23:43:32 -0300

    Changed related names of author and watched in video

--- VinVC/video/models.py
+++ VinVC/video/models.py
@@ -8,8 +8,7 @@ from .validators import validate_file_extension
 class Video(models.Model):
     title = models.CharField(blank=True, max_length=100)
     author = models.ForeignKey(settings.AUTH_USER_MODEL,
-                               on_delete=models.CASCADE,
-                               related_name='author')
+                               on_delete=models.CASCADE)
     date_upload = models.DateTimeField('date upload', default=timezone.now)
     video_file = models.FileField(upload_to="videos/%Y/%m/%d",
                                   validators=[validate_file_extension])
@@ -19,15 +18,15 @@ class Video(models.Model):
     watchers = models.ManyToManyField(settings.AUTH_USER_MODEL,
                                       through='WatchingVideo',
                                       through_fields=('video', 'user'),
-                                      symmetrical=True)
+                                      symmetrical=True,
+                                      related_name='watched')
 
     def __str__(self):
         return "Title: {}, Description: {}, Author: {}, Date: {}, File: {}, " \
-               "Views: {}, Watchers: {}, Thumbnail: {}".format(self.title, self.description,
-                                                               self.author, self.date_upload,
-                                                               self.video_file.name,
-                                                               self.views, self.watchers.all(),
-                                                               self.thumbnail)
+               "Views: {}, Watchers: {}, Thumbnail: {}".\
+            format(self.title, self.description, self.author, self.date_upload,
+                   self.video_file.name, self.views, self.watchers.all(),
+                   self.thumbnail)
 
 
 class WatchingVideo(models.Model):
@@ -37,3 +36,8 @@ class WatchingVideo(models.Model):
     video = models.ForeignKey(Video, on_delete=models.CASCADE,
                               related_name='video')
     date = models.DateTimeField('date view', default=timezone.now)
+
+    def __str__(self):
+        return "User: {}, Video: <<{}>>, Date: {}".format(self.user,
+                                                          self.video,
+                                                          self.date)

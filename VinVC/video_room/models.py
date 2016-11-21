ca89af4d935319e06d66ec051b4c4375e3cdbaf8
Agustín Curto <agucurto95@gmail.com>
2016-11-21 20:01:47 -0300

    Changed VideoRoom model

--- VinVC/video_room/models.py
+++ VinVC/video_room/models.py
@@ -3,8 +3,16 @@ from video.models import Video
 
 
 class VideoRoom(models.Model):
-    paused = models.BooleanField()
+    PAUSED = 0
+    PLAYING = 1
+
+    STATUS = (
+        (PLAYING, 'Playing'),
+        (PAUSED, 'Paused'),
+    )
+
+    status = models.IntegerField(choices=STATUS, default=PAUSED)
     video = models.ForeignKey(Video, on_delete=models.CASCADE)
 
     def __str__(self):
-        return "Video: {}, Paused: {}".format(self.video, self.paused)
+        return "Video: {}, Status: {}".format(self.video, self.status)

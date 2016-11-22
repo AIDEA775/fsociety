2ddb6880b6d8af57ea26d3b3bf63af4a3b24912b
Mauricio Guignard <mauguignard@gmail.com>
2016-11-22 00:35:58 -0300

    Updated VideoRoom model

--- VinVC/video_room/models.py
+++ VinVC/video_room/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.utils import timezone
 from video.models import Video
 
 
 class VideoRoom(models.Model):
     paused = models.BooleanField()
     video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    started_playing = models.DateTimeField('started playing', default=timezone.now)
+    current_time = models.IntegerField(default=0)
 
     def __str__(self):
         return "Video: {}, Paused: {}".format(self.video, self.paused)
